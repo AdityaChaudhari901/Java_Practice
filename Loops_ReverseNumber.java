6d036fd6b7bc2cafa3828a7a7555f613267d763c
Aditya Chaudhari <AdityaChaudhari901@gmail.com>
2024-07-25 13:07:42 +0530

    one

---
+++ Loops_ReverseNumber.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Loops_ReverseNumber {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        while (n!=0) {
+            int Lastdigit = n%10;
+            System.out.print( Lastdigit);
+            
+            n = n/10;
+        }
+       
+    }
+}
