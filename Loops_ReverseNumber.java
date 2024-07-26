07c28090882643759dae82fc1d27bd8a35fb9450
Aditya Chaudhari <AdityaChaudhari901@gmail.com>
2024-07-26 15:57:28 +0530

    gg

--- Loops_ReverseNumber.java
+++ Loops_ReverseNumber.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 
 public class Loops_ReverseNumber {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        while (n!=0) {
-            int Lastdigit = n%10;
-            System.out.print( Lastdigit);
-            
-            n = n/10;
+        try (Scanner scn = new Scanner(System.in)) {
+            int n = scn.nextInt();
+            while (n!=0) {
+                int Lastdigit = n%10;
+                System.out.print( Lastdigit);
+                
+                n = n/10;
+            }
         }
        
     }
